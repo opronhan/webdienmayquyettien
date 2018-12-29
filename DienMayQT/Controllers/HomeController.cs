a0cc5fc84650e0976dbb1911f37ff0564c194401
phucstorm <t163806@vanlanguni.vn>
2018-12-30 06:26:53 +0700

    Create Front-end Gui and show product from db

--- DienMayQT/Controllers/HomeController.cs
+++ DienMayQT/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DienMayQT.Models;
 
 namespace DienMayQT.Controllers
 {
     public class HomeController : Controller
     {
+        DmQT09Entities db = new DmQT09Entities();
         public ActionResult Index()
         {
-            return View();
+            var product = db.Products.OrderByDescending(x => x.ID).ToList();
+            return View(product);
         }
 
         public ActionResult About()
